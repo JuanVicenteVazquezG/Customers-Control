4d3a6e91f580bf1bf15accf28a9650d71b44781e
Juan Vicente Vazquez <juanvicentevazquezg@gmail.com>
2020-11-09 16:08:24 +0100

    added modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BoardComponent } from './components/board/board.component';
+import { CustomersComponent } from './components/customers/customers.component';
+import { CustomerEditComponent } from './components/customer-edit/customer-edit.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { SettingComponent } from './components/setting/setting.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BoardComponent,
+    CustomersComponent,
+    CustomerEditComponent,
+    LoginComponent,
+    RegistrationComponent,
+    SettingComponent,
+    NotFoundComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
